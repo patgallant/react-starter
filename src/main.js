1b8cc3dbb5203963b5a2e4943eed824275adea0a
Pat Gallant <pat.gallant@gmail.com>
2017-12-08 13:27:30 -0500

    Added comment to main.js (to test git integration with VS Code).

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Counter from "./Counter";
 
+/**
+ * Add event listener to attach React component into the DOM.
+ */
 document.addEventListener(
     "DOMContentLoaded", 
     () => {
